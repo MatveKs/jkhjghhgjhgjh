535e8f949590e33bb116e71a2fce8383633d25fe
MatveKs <104572993+MatveKs@users.noreply.github.com>
2022-11-21 22:30:02 +0200

    спроба
    спроба

---
+++ javascript.js
@@ -0,0 +1,36 @@
+
+
+
+$ (document)
+
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+  loop:true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
+$('.project').magnificPopup({
+  delegate:'a',
+  type:'image',
+  gallery:{
+    enabled:true,
+  }
+
+});
+
+
+
+
